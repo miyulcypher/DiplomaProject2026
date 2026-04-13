5145da21ba082787ecef458ec7f3c0cb06b693ef
miyulcypher <amirulhafizaziz07@gmail.com>
2026-04-13 19:20:56 +0800

    13/4/2026

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.yellow,
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey,
-          title: Center(
-            child: Text('HELLO USAS'),
-          ),
-        ),
-        body: Center(
-          child: Text(
-            'MUHAMMAD AMIRUL HAFIZ',
-            style: TextStyle(fontSize: 50),
+        backgroundColor: Colors.deepPurple,
+        body: SafeArea(
+          child: Container(
+            alignment: Alignment.center,
+            width: 350,
+            height: 250,
+            margin: EdgeInsets.symmetric(
+              vertical: 50,
+              horizontal: 50,
+            )
+            ,color: Colors.white,
+            child: Text(
+                'MUHAMMAD AMIRUL HAFIZ',
+              style: TextStyle(
+                  fontSize: 25),
+              textAlign: TextAlign.center,
+            ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
