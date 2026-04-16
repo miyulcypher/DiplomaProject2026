6975a7828c4974658b58f4342514167d16add698
miyulcypher <amirulhafizaziz07@gmail.com>
2026-04-16 19:14:38 +0800

    16/4/2026

--- lib/main.dart
+++ lib/main.dart
@@ -9,24 +9,109 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.deepPurple,
+        backgroundColor: Colors.lightBlueAccent,
         body: SafeArea(
-          child: Container(
-            alignment: Alignment.center,
-            width: 350,
-            height: 250,
-            margin: EdgeInsets.symmetric(
-              vertical: 50,
-              horizontal: 50,
-            )
-            ,color: Colors.white,
-            child: Text(
-                'MUHAMMAD AMIRUL HAFIZ',
-              style: TextStyle(
-                  fontSize: 25),
-              textAlign: TextAlign.center,
-            ),
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.center,
+                width: 500,
+                height: 100,
+                margin: EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 10,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Row(
+                  children: [
+                    SizedBox(width: 10),
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage('images/images.jpg'),
+                    ),
+                    SizedBox(width: 15),
+                    Icon(Icons.adb),
+                    Expanded(
+                      child: Text(
+                        'MUHAMMAD AMIRUL HAFIZ',
+                        style: TextStyle(fontSize: 20,
+                        fontFamily: 'Pacifico'),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: 500,
+                height: 100,
+                margin: EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 10,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Row(
+                  children: [
+                    SizedBox(width: 10),
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage('images/cat-cute-cat.gif'),
+                    ),
+                    SizedBox(width: 15),
+                    Icon(Icons.ac_unit),
+                    Expanded(
+                      child: Text(
+                        'Rynn',
+                        style: TextStyle(fontSize: 30,
+                            fontFamily: 'Noto Serif Display'),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: 500,
+                height: 100,
+                margin: EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 10,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Row(
+                  children: [
+                    SizedBox(width: 10),
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage('images/download (3).jpg'),
+                    ),
+                    SizedBox(width: 15),
+                    Icon(Icons.ramen_dining),
+                    Expanded(
+                      child: Text(
+                        'Ada Wong',
+                        style: TextStyle(fontSize: 30,
+                            fontFamily: 'Saira Stencil'),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
